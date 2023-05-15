e603fc548eb23a7e3134ab0a85b2be5dbd895cb7
pp <ltylzq@163.com>
2023-05-15 20:52:58 +0800

    init

---
+++ src/main/java/org/mini/spring/io/Main.java
@@ -0,0 +1,15 @@
+package org.mini.spring.io;
+
+/**
+ * <p>
+ *     TODO
+ * </p>
+ *
+ * @author ${USER}
+ * @since ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
